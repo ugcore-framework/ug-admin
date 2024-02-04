606087b39ddb1f9cf2176a62ccdc2e0a6b3d7717
UgDev <support@ugdev.cf>
2024-02-04 17:50:28 +0100

    Updated the URL GitHub Repository

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ name 'ug-admin'
 description 'Admin Script for UgCore Framework By UgDev'
 author 'UgDev'
 version '3.5'
-url 'https://github.com/UgDevOfc/ug-admin'
+url 'https://github.com/ugcore-framework/ug-admin'
 
 shared_scripts {
     '@ug-core/languages.lua',
